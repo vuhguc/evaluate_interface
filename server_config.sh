a2407092d021c1894adfc16714249cd0d85d937c
phuchoang1308 <phuchoang1308@gmail.com>
2020-05-01 00:07:46 -0400

    fix bugs

--- server_config.sh
+++ server_config.sh
@@ -1,17 +1,20 @@
+#!/bin/bash
 setenforce 0
-CUR_PATH = $(dirname '$(readlink -f "$0")')
-chown -R apache:apache "$CUR_PATH"
-chmod -R a=r,u+w,a+X "$CUR_PATH"
+CUR_PATH=$(realpath "$0")
+CUR_DIR=$(dirname "$CUR_PATH")
+chown -R apache:apache "$CUR_DIR"
+chmod -R a=r,u+w,a+X "$CUR_DIR"
+chmod 755 "$CUR_PATH" "$CUR_DIR/server_app/main.py"
 cat > /etc/httpd/conf.d/evaluate_interface.conf <<EOF
 LoadModule wsgi_module modules/mod_wsgi.so
 
-WSGIScriptAlias /evaluate_interface "$CUR_PATH/server_app/main.py/"
+WSGIScriptAlias /evaluate_interface "$CUR_DIR/server_app/main.py/"
 
-Alias /evaluate_interface/static "$CUR_PATH/server_app/static/"
+Alias /evaluate_interface/static "$CUR_DIR/server_app/static/"
 AddType text/html .py
 
-<Directory $CUR_PATH/>
-  Order deny, allow
+<Directory "$CUR_DIR/">
+  Order deny,allow
   Allow from all
 </Directory>
 EOF
